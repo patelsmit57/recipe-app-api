f77e1068718d450b24ac28919f11899cd8b11615
Patel smit <patelsmit15150@gmail.com>
2022-07-20 17:57:29 +0530

    whitespace

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,13 @@
 """
 Calculator funtions
 """
+
+
 def add(x, y):
     """Add x and y return result."""
     return x+y
+
+
 def subtract(x, y):
     """subtract y from x return result."""
     return y-x
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,6 +9,7 @@ from django.db.utils import OperationalError
 
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for database."""
 
@@ -23,5 +24,5 @@ class Command(BaseCommand):
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-                
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
